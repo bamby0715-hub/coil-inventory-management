85af42cb30f5c3a0025a16013bd412da81bf3818
bamby0715-hub <bamby000715@gmail.com>
2026-06-26 14:46:38 +0900

    Delete public/firebase-config.js

--- public/firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.HNMT_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB9A8MD4pndOPDh2BmplB-Hi0xDF8fOBqM",
-  authDomain: "h-coil.firebaseapp.com",
-  projectId: "h-coil",
-  storageBucket: "h-coil.firebasestorage.app",
-  messagingSenderId: "44494471464",
-  appId: "1:44494471464:web:260966089dfc550419d01d",
-  measurementId: "G-HWSENJ8JWD"
-};
